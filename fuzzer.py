4681e89bb09eb896a36564ae4ef6660c940ab48e
brend3n <brenden@knights.ucf.edu>
2022-03-06 21:08:03 -0500

    Trying to figure out how to modify bytes either using bytearray or list or something

--- fuzzer.py
+++ fuzzer.py
@@ -1,20 +1,39 @@
 import sys
 import os
 import time
+import random
 
 # Modify the jpg pseudo-randomly to trigger bugs
 def mutate(iteration, output_filename):
-    
+
+    bytes_d = None
+
     # Read File
     with open("cross.jpg", "rb") as jpg:
-        bytes_d = jpg.read()
+        bytes_d = bytearray(jpg.read())
 
-    # Do mutations here
-    print("Mutation.")
+    print(f"length of file: {len(bytes_d)}")
+
+    num_modifications = random.randint(1,len(bytes_d)-4)
+    print(f"num_modifications: {num_modifications}")
+
+    print(bytes_d)
     
+    # ! Need to figure out how to modify bytes
+    # Make modifications not on header and footer though (between bytes 2-len(bytes)-2)
+    for i in range(num_modifications):
+        rand_pos = random.randint(2,len(bytes_d)-2) 
+        rand_byte_replacement = "A"
+        bytes_d[rand_pos] = rand_byte_replacement
+
+
     print(f"\nbytes:\n{bytes_d}\n")
 
     filename = f"./{output_filename}/cross_mutated_{iteration}.jpg"
+
+    # Convert back to bytes
+    bytes_d = bytes(bytes_d)
+
     # Writing mutated bytes to file
     with open(filename, "wb") as jpg:
         jpg.write(bytes_d)
