0704db209046dfc787ea9a32c0738b589ccc1ebd
brend3n <brenden@knights.ucf.edu>
2022-03-07 21:10:32 -0500

    GooAble to modify bytes positions as well randomly... Already finding bugs

--- fuzzer.py
+++ fuzzer.py
@@ -3,35 +3,53 @@ import os
 import time
 import random
 
+def get_random_byte_replacement():
+    return int.from_bytes(os.urandom(1), 'big')
+    
+
 # Modify the jpg pseudo-randomly to trigger bugs
 def mutate(iteration, output_filename):
 
+    # Variable to store bytes
     bytes_d = None
-
+    
     # Read File
     with open("cross.jpg", "rb") as jpg:
-        bytes_d = bytearray(jpg.read())
-
-    print(f"length of file: {len(bytes_d)}")
-
-    num_modifications = random.randint(1,len(bytes_d)-4)
-    print(f"num_modifications: {num_modifications}")
-
-    print(bytes_d)
+        bytes_d = jpg.read()
+        
+    # Convert to list to modify file data
+    bytes_d = list(bytes_d)
     
-    # ! Need to figure out how to modify bytes
+    '''
+        #!
+        this is how to modify a byte in the list
+        bytes_d[0] = \xFE 
+        #!
+    '''
+    
+    # Number of modifications to make
+    # num_modifications = random.randint(1,len(bytes_d)-4)
+    num_modifications = 1
+    
+    # print(f"num_modifications: {num_modifications}")
+
     # Make modifications not on header and footer though (between bytes 2-len(bytes)-2)
     for i in range(num_modifications):
-        rand_pos = random.randint(2,len(bytes_d)-2) 
-        rand_byte_replacement = "A"
+        
+        # Random position to modify
+        rand_pos = random.randint(2,len(bytes_d)-2)
+        
+        # Get random byte
+        rand_byte_replacement = get_random_byte_replacement()
+        
+        # Modify byte
         bytes_d[rand_pos] = rand_byte_replacement
 
-
-    print(f"\nbytes:\n{bytes_d}\n")
+    # print(f"\nbytes:\n{bytes_d}\n")
 
     filename = f"./{output_filename}/cross_mutated_{iteration}.jpg"
 
-    # Convert back to bytes
+    # Convert back to byte string to write to file
     bytes_d = bytes(bytes_d)
 
     # Writing mutated bytes to file
@@ -60,6 +78,7 @@ def run():
         curr_file = f"cross_mutated_{i}"
         print(f"Current Mutation: {curr_file}")
         mutate(i, time_folder)
+
         # Create command string to execute
         exec_str = f'./jpgbmp ./{time_folder}/{curr_file}.jpg ./{time_folder}/{curr_file}.bmp'
         os.system(exec_str)
